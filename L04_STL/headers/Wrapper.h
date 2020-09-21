fb970bb6440bdafeb5733360f731de59963d3cda
Your Name <you@example.com>
2020-09-21 14:03:00 +0200

    Wrapper on its way

--- L04_STL/headers/Wrapper.h
+++ L04_STL/headers/Wrapper.h
@@ -8,11 +8,19 @@
 #include <functional>
 
 template<typename T>
-class Wrapper
+class Wrapper : public std::iterator<std::input_iterator_tag, T>
 {
 public:
-    Wrapper(std::function<void(int)> objFunctor) : wrapperFunctor(objFunctor){
-    }
+    Wrapper(std::function<void(T)> objFunctor) : wrapperFunctor(objFunctor){}
+    Wrapper(const Wrapper& toCopy) : wrapperFunctor(toCopy.wrapperFunctor) {}
+    Wrapper& operator=(const Wrapper& rhs) {wrapperFunctor = rhs.wrapperFunctor; return *this; }
+    Wrapper& operator=(const T& value){return *this; }
+    Wrapper& operator++() {return *this;}
+    Wrapper& operator++(int) { return *this; }
+    bool operator==(const Wrapper& rhs) const {return wrapperFunctor == rhs.wrapperFunctor;}
+    bool operator!=(const Wrapper& rhs) const {return wrapperFunctor != rhs.wrapperFunctor;}
+    Wrapper& operator*() {return *this; }
+
     void operator()(T value) const {
         wrapperFunctor(value);
     }
